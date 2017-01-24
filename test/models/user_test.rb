db95a917280019e00f451bd1242dfb7dfc80f55d
Agis Anastasopoulos <agis@skroutz.gr>
2017-01-24 11:16:02 +0200

    Update test case according to described issue

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,12 +2,22 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "foo" do
-    User.create!
+	  assert_equal 0, User.count
+	  assert_equal 0, User.on_slave.count
 
-    assert_equal 1, User.count # succeeds
-    assert_equal 1, User.on_slave.count # fails
+	  # this happens in a transaction in `master` connection (let's call it connection A)
+    # but is *not* yet commited, therefore only connection `A` knows about this change
+	  User.create!
 
-    User.connection.commit_db_transaction
-    assert_equal 1, User.on_slave.count # succeeds
+	  # this succeeds since it runs in A
+	  assert_equal 1, User.count
+
+	  # this fails since it runs in another connection (let's call it `B`) which
+    # has no knowledge of
+	  # changes in A (since that transaction is not commited yet)
+	  assert_equal 1, User.on_slave.count
+
+	  User.connection.commit_db_transaction
+	  assert_equal 1, User.on_slave.count # succeeds
   end
 end
