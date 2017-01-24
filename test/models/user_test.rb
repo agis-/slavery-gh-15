aa521a94a5dfd2a9010ad733fa27ce2f0ed90b6f
Agis Anastasopoulos <agis@skroutz.gr>
2017-01-24 11:06:19 +0200

    Further demonstrate the issue

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,10 @@ class UserTest < ActiveSupport::TestCase
   test "foo" do
     User.create!
 
-    assert_equal 1, User.count
+    assert_equal 1, User.count # succeeds
     assert_equal 1, User.on_slave.count # fails
+
+    User.connection.commit_db_transaction
+    assert_equal 1, User.on_slave.count # succeeds
   end
 end
